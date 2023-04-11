0e090b8d665932f180bf2318d2b06336478dd77b
immortallz <129889191+immortallz@users.noreply.github.com>
2023-04-12 00:51:42 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 from scipy.stats import ks_2samp
+from hyppo.ksample import Energy, MMD, DISCO
 
 chat_id = 388568881 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    if ks_2samp(x, y, alternative="two-sided").pvalue < 0.09:
+    if MMD(compute_kernel="laplacian", gamma=1).test(x, y)[1] > 0.09:
         return False
     else:
         return True
