f310b9e6dce18b2d07cdceae26304f7c53a3a7d8
immortallz <129889191+immortallz@users.noreply.github.com>
2023-04-11 23:36:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 388568881 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     if ks_2samp(x, y, alternative="two-sided").pvalue < 0.09:
