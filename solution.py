3d180354bb863f9d191de2753ed54b19aead6bfa
immortallz <129889191+immortallz@users.noreply.github.com>
2023-04-11 23:21:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    if ks_2samp(x, y, alternative="two-sided").pvalue < 0.09:
+        return False
+    else:
+        return True
